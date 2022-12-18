daca2251ca5b60b778400f13b54b1ed7e63be97d
arotesaurabh <saurabharote35@gmail.com>
2022-12-18 08:16:02 -0800

    java script

---
+++ java script 1/script.js
@@ -0,0 +1,69 @@
+alert("hi gaurav")
+
+
+// 1. declare a variable with suitable value 
+
+// var myName = "hi gaurav";     
+// console.log(myName);
+
+//2. Assign value(datatype) of variable 
+
+// var data base = 26;
+// console.log(data base);
+
+// //3. let keyword & declare variable 
+
+// let java =17;
+// console.log(java)
+
+// 4.create variable z and asign x+y to it & show in alert box
+
+// var x=56;
+// var y=19;     
+// var z = x+y;
+// console.log(z); 
+
+// 5.create variable x and asssign value 50 
+
+// var x=100;
+// console.log(x);
+
+// 6.multiply 10 with 5 and alert result
+
+// var x=668;
+// var y=47;
+// var z=x*y;
+// console.log(z);
+
+// 7. divided 10 by 2 and alert result
+
+// var x=265;
+// var y=266;
+// var z=x/y;
+// console.log(z);
+
+// 8.use correct assingnment operator that result in x being 15 (same as x=x+y)
+
+// var x=25;
+// console.log(x+=y);
+
+
+//datatype - string
+let a="30";
+console.log(typeof a);      
+
+//  datatype - number
+let j=34;
+console.log(typeof j);   
+
+//  datatype datatype - boolean
+let m= true;
+console.log(typeof m);     
+
+// datatype - null
+let s= null;
+console.log(s);     
+
+// datatype - undefined
+let d;
+console.log(d);
